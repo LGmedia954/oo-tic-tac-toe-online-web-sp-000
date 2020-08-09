1a6ae9c0ce2d978cfd86e39c5a0875b0a17175df
Leslie G <lesliegonzalez@me.com>
2020-08-09 15:07:34 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,6 +16,16 @@ def initialize(board = nil)
   @board = board || Array.new(9, " ")
 end
 
+def display_board
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+
+
 def current_player
   turn_count % 2 == 0 ? "X" : "O"
 end
@@ -24,13 +34,7 @@ def turn_count
   @board.count{|token| token == "X" || token == "O"}
 end
  
-def display_board
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
+
   
   
 end
