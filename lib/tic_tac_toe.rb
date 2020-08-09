4864c57698c59896448dac1b2ae1df536f673de8
Leslie G <lesliegonzalez@me.com>
2020-08-09 16:02:00 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -63,7 +63,7 @@ end
  
 def won?
   WIN_COMBINATIONS.any? do {|combo|
-    index_0 = win_combo[0]
+    index_0 = win_
     index_1 = win_combo[1]
     index_2 = win_combo[2]
 
@@ -71,10 +71,7 @@ def won?
     position_2 = @board[index_1]
     position_3 = @board[index_2]
 
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combo
-    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return win_combo
+    if position_taken(combo[0]) && 
       
       return combo
     end
