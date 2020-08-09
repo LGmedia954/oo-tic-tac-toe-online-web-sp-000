7390084d9a27e96f7738c354bc9dd7a6c98bb2a9
Leslie G <lesliegonzalez@me.com>
2020-08-09 16:04:18 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -62,8 +62,7 @@ def current_player
 end
  
 def won?
-  WIN_COMBINATIONS.any? do {|combo|
-
+  WIN_COMBINATIONS.any? do |combo|
     if position_taken?(combo[0]) && @board[combo[0]] && @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
       
       return combo
