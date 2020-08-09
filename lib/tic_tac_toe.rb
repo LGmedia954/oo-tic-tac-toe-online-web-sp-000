1102037c9984c5804ea059a3477bcb01c81d7cc6
Leslie G <lesliegonzalez@me.com>
2020-08-09 15:53:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -28,8 +28,8 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(index, player_token)
-  @board[index] = player_token
+def move(index, token)
+  @board[index] = token
 end
 
 def position_taken?(index)
