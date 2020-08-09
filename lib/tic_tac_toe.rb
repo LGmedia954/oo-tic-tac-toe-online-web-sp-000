6f96bc763a0a412071edb9c261c2f68a8b417c57
Leslie G <lesliegonzalez@me.com>
2020-08-09 15:01:12 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -13,7 +13,7 @@ def turn_count
   @board.count{|token| token == "X" || token == "O"}
 end
  
-def display_board(board)
+def display_board
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
